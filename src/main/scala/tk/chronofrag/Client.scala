355e3e12bac650288b16977a7e082d548d1001ca
jemmele06 <jemmele06@yahoo.com>
2015-06-26 14:16:41 -0500

    Added comments to classes, objects, functions. Added TODOs for work to be included in the next release.

--- src/main/scala/tk/chronofrag/Client.scala
+++ src/main/scala/tk/chronofrag/Client.scala
@@ -5,17 +5,22 @@ import java.net.InetSocketAddress
 import akka.actor.{Actor, ActorRef, Props}
 import akka.io.{IO, Tcp}
 
-case class IncomingRawMessage(prefix: UserMask, command: String, destination: String, message: String)
+case class RawMessage(prefix: UserMask, command: String, destination: String, message: String)
 case class UserMask(nick: String, name: String, hostmask: String)
 
 /**
-* Client actor establishes connection to IRC server and launches subsystems to handle incoming messages.
-*/
+ * Client companion object to declare props with remote server InetSocketAddress
+ */
 object Client {
   def props(remote: InetSocketAddress) =
     Props(classOf[Client], remote)
 }
 
+/**
+ * Client actor establishes connection to IRC server and launches subsystems to handle incoming messages. Initial
+ * implementation taken from Akka TCP documentation.
+ * @param remote InetSocketAddress to the IRC server
+ */
 class Client(remote: InetSocketAddress) extends Actor {
 
   import Tcp._
@@ -47,17 +52,25 @@ class Client(remote: InetSocketAddress) extends Actor {
       }
   }
 
-  def process(connection: ActorRef, data: String): IncomingRawMessage = {
+  /**
+   * Processes incoming messages from the server that match RFC 1459 and 2812 for IRC messages. Only to be used after a 
+   * successful connection to the server is established.
+   * @param connection ActorRef to the TCP actor.
+   * @param data String that contains a single line from the IRC server.
+   * @return RawMessage instance
+   */
+  def process(connection: ActorRef, data: String): RawMessage = {
+    //TODO - Regex allows null values for unmatched groups. Possible fixes are: (1) wrap in Option (2)remake Regex (3) let null leak
     //Regex taken from: http://www.mybuddymichael.com/writings/a-regular-expression-for-irc-messages.html
     val IrcRegex = "^(?:[:](\\S+) )?(\\S+)(?: (?!:)(.+?))?(?: [:](.+))?$".r
     val IrcRegex(prefix, command, destination, message) = data
 
     val UserMaskRegex = "([^!]*)!?([^@]*)?@?(.*)?".r
-    val UserMaskRegex(nickname, username, dns) = Option(prefix) getOrElse ""
+    val UserMaskRegex(nickname, username, dns) = Option(prefix) getOrElse "" //if prefix null, replace with empty string
     val usermask = UserMask(nickname, username, dns)
 
-    println(s"p($usermask) c($command) d($destination) m($message)")
-    new IncomingRawMessage(usermask, command, destination, message)
+    println(s"U($usermask)C($command)D($destination)M($message)")
+    new RawMessage(usermask, command, destination, message)
   }
 }
 
--- src/main/scala/tk/chronofrag/CommandActor.scala
+++ src/main/scala/tk/chronofrag/CommandActor.scala
@@ -4,18 +4,28 @@ import akka.actor.{Actor, ActorRef}
 import akka.io.Tcp.Write
 import akka.util.ByteString
 
-
+/**
+ * CommandActor receives RawMessages and pattern matches over the command.
+ * @param connection ActorRef to the TCP actor.
+ */
 class CommandActor(connection: ActorRef) extends Actor {
+  //TODO - Add support for user defined values. Get rid of initial debugging strings!
   val (userMsg, nickMsg, joinMsg) = (
     "tyrname tyrrealname localhost :JRaccoon",
     "tyrbot5",
     "#achrontest")
 
   def receive = {
-    case IncomingRawMessage(_, "PING", _, message) =>
+    /**
+     * PING - Each PING should be responded to with a matching PONG
+     */
+    case RawMessage(_, "PING", _, message) =>
       pong(message)
 
-    case IncomingRawMessage(_, "NOTICE", destination, message) => destination match {
+    /**
+     * NOTICE - Used to determine when the server begins communication
+      */
+    case RawMessage(_, "NOTICE", destination, message) => destination match {
       case "AUTH" => message match {
         case s: String if s.contains("Looking up your hostname") =>
           user(userMsg)
@@ -25,38 +35,71 @@ class CommandActor(connection: ActorRef) extends Actor {
       case _ => //Silently ignore other destinations from NOTICE commands
     }
 
-    case IncomingRawMessage(_, "376", _, _) => //End of MOTD, safe to send
+    /**
+     * 376 - Numeric command response signaling the end of MOTD. After this it should be safe to send JOIN messages.
+     */
+    case RawMessage(_, "376", _, _) =>
       join(joinMsg)
 
-    case IncomingRawMessage(user, "PRIVMSG", dest, msg) =>
-      if(dest == nickMsg)
+    /**
+     * PRIVMSG - If message is received, echo the message back to the user or channel that sent it.
+     */
+    case RawMessage(user, "PRIVMSG", dest, msg) =>
+      if(dest == nickMsg) //If sent directly to this bot, reply to directly to the sending user
         say(msg, user.nick)
-      else
+      else //Else it must have been sent to a channel the bot is in; reply to that channel.
         say(msg, dest)
 
     case _ => //Silently ignore other commands
   }
 
+  /**
+   * sendRaw wraps a String in a ByteString which is wrapped in a TCP.Write and finally sent to the TCP connection
+   * actor.
+   * @param message String that should be sent to the server
+   */
   def sendRAW(message: String): Unit = {
+    //TODO - String should be escaped before sending
     connection ! Write(ByteString(message + "\n"))
   }
 
+  /**
+   * pong sends a raw PONG IRC message
+   * @param message Message that should be sent as part of PONG response.
+   */
   def pong(message: String): Unit = {
     sendRAW(s"PONG :$message")
   }
 
+  /**
+   * user sends a raw USER IRC message
+   * @param user Message that should be sent as part of USER message. Should be in format "<username> <hostname> <servername> <realname>"
+   */
   def user(user: String): Unit = {
     sendRAW(s"USER $user")
   }
 
+  /**
+   * nick sends a raw NICK IRC message
+   * @param nick Message that should be sent as part of NICK message. Should conform to IRC nickname creation.
+   */
   def nick(nick: String): Unit = {
     sendRAW(s"NICK $nick")
   }
 
+  /**
+   * join sends a raw JOIN IRC message
+   * @param channel Message that should be sent as part of JOIN message. Represents the channel list to join.
+   */
   def join(channel: String): Unit = {
     sendRAW(s"JOIN $channel")
   }
 
+  /**
+   * say sends a raw PRIVMSG IRC message to the given destination
+   * @param msg Message that should be sent as part of PRIVMSG message.
+   * @param dest Nick of the user or channel to send message.
+   */
   def say(msg: String, dest: String): Unit = {
     sendRAW(s"PRIVMSG $dest :$msg")
   }
